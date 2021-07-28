7d2f3c020cc15df4cfab5bddddeed56c82214d1c
Jinxuan Hao <2547325h@student.gla.ac.uk>
2021-07-28 14:24:24 +0100

    chapter4 add index.html

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -14,9 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print(__file__)
-print(os.path.dirname(__file__))
-print(os.path.dirname(os.path.dirname(__file__)))
+# print(__file__)
+# print(os.path.dirname(__file__))
+# print(os.path.dirname(os.path.dirname(__file__)))
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
